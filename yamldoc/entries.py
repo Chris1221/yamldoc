edf52f7d1faf958426c2071f0fa51ada992d90ec
Christopher Cole <ccole@well.ox.ac.uk>
2023-06-26 07:50:11 -0400

    feat: selectively exclude entries fix #12 (#13)
    * feat: selectively exclude entries
    
    * fix: remove bad import
    
    * fix: allow for nested lists

--- yamldoc/entries.py
+++ yamldoc/entries.py
@@ -2,13 +2,41 @@ import textwrap
 from dataclasses import dataclass
 
 
+def sanitize_meta(meta, char, exclude_char="#'!", override_exclude=False):
+    """
+    Sanitizes the meta information and indicates
+    whether or not the entry should be excluded.
+
+    Arguments:
+        meta: The meta information to sanitize.
+        override_exclude: Override the exclusion character and force inclusion.
+    """
+
+    meta = meta.lstrip().rstrip()
+
+    if meta.startswith(exclude_char):
+        exclude = True
+    else:
+        exclude = False
+
+    if meta.startswith(exclude_char):
+        meta = meta.replace(exclude_char, "").lstrip()
+    elif meta.startswith(char):
+        meta = meta.replace(char, "").lstrip()
+
+    if override_exclude:
+        exclude = False
+
+    return meta, exclude
+
+
 class MetaEntry:
     """
     A container to hold a base level YAML entry plus any associated
     hierarchical keys and values.
     """
 
-    def __init__(self, name, meta):
+    def __init__(self, name, meta, char, exclude_char="#'!", override_exclude=False):
         """
         Initialize the object.
 
@@ -17,31 +45,69 @@ class MetaEntry:
             meta: Comments derived from YAML file.
         """
         self.name = name
-        self.meta = meta
+        self.char = char
+        self.exclude_char = exclude_char
         self.isBase = True
         self.entries = []
         self.has_schema = False
 
+        self.meta, self.exclude = sanitize_meta(meta, char, exclude_char, override_exclude)
+
     def is_list(self):
         """Returns True if all elements are list elements and False otherwise."""
         return all([isinstance(entry, ListElement) for entry in self.entries])
-    
+
     def to_list_entry(self):
         """Converts this meta instance to a base level list entry."""
         if self.is_list():
             values = [entry.entry for entry in self.entries]
+            entry = Entry(self.name, values, self.meta, self.char, self.exclude_char)
+            
+            # Small detail here, the meta has already been parsed
+            # so we don't want to do it again.
+            if self.exclude:
+                entry.exclude = True
 
-            return Entry(self.name, values, self.meta)
-
+            return entry
 
     def __repr__(self):
         """
         Returns a print representation.
         """
         if self.has_schema:
-            return f"YAML Meta Object with {len(self.entries)} entries [{self.name}] and type information."
+            return (
+                f"Meta object (n = {len(self.entries)}) with schema: {self.entries}" )
         else:
-            return f"YAML Meta Object with {len(self.entries)} entries [{self.name}]"
+            return f"Meta object (n = {len(self.entries)}) without schema: {self.entries}"
+
+    def non_excluded_entries(self):
+        """Returns a list of entries that are not excluded."""
+        return [entry for entry in self.entries if not entry.exclude]
+
+    def table_header(self, schema=False):
+        if schema:
+            header = textwrap.dedent("""
+            | Key | Value | Type | Information |
+            | :-: | :-: | :-: | :-- |
+            """)
+        else:
+            header = textwrap.dedent("""
+            | Key | Value | Information |
+            | :-: | :-: | :-- |
+            """)
+
+        return header
+    
+    def check_for_lists(self):
+        new_entries = []
+        for entry in self.entries:
+            if isinstance(entry, MetaEntry):
+                if entry.is_list():
+                    new_entries.append(entry.to_list_entry())
+                    continue
+            new_entries.append(entry)
+        
+        self.entries = new_entries
 
     def to_markdown(self, schema=False):
         """
@@ -50,40 +116,49 @@ class MetaEntry:
         Argumenets:
             schema: Print with four columns instead of three.
         """
-        if schema:
-            output = f"## `{self.name}`\n\n{self.meta.lstrip()}\n\n"
-            output += "### Member variables:\n\n"
 
-            output += "| Key | Value | Type | Information |\n"
-            output += "| :-: | :-: | :-: | :-- |\n"
+        # If the object is excluded, we don't want to print anything.
+        if self.exclude:
+            return ""
+        
+        # Check for any sublists that need to be converted
+        # from meta to entries
+        self.check_for_lists()
+        
+        # Regardles of whether or not there are entries to print, we still want to print the
+        # meta information.
+        output = f"## `{self.name}`\n\n{self.meta.lstrip()}\n\n"
 
-            for entry in self.entries:
-                output += entry.to_markdown(schema) + "\n"
-            return output
-
-        else:
-            output = f"## `{self.name}`\n\n{self.meta.lstrip()}\n\n"
-            output += "### Member variables:\n\n"
-
-            output += "| Key | Value | Information |\n"
-            output += "| :-: | :-: | :-- |\n"
-
-            for entry in self.entries:
-                output += entry.to_markdown() + "\n"
+        # This is an early exit if there are no entries to print.
+        entries_to_print = self.non_excluded_entries()
+        if len(entries_to_print) == 0:
+            output += "No member variables.\n\n"
 
             return output
+
+        # So we have entries to print. Let's print them.
+        output += "### Member variables:\n\n"
+        output += self.table_header(schema)
+
+        for entry in entries_to_print:
+            output += entry.to_markdown(schema) + "\n"
+
+        return output
 
 
 @dataclass
 class ListElement:
     entry: str
+    exclude: bool = False 
 
 
 class Entry:
     """
     Container for a single YAML key value pairing and associated metadata."""
 
-    def __init__(self, key, value, meta):
+    def __init__(
+        self, key, value, meta, char="#'", exclude_char="#'!", override_exclude=False
+    ):
         """
         Initialize the object
 
@@ -91,19 +166,30 @@ class Entry:
            key: Name of the value
            value: Given value.
            meta: Any associated comments or meta data.
+            char: Character to denote meta data.
+            exclude_char: Character to denote exclusion.
+            override_exclude: Override the exclusion character and force inclusion.
         """
         self.key = key
         self.value = value
-        self.meta = meta
+        self.char = char
+        self.exclude_char = exclude_char
         self.isBase = False
         self.type = None
+
+        self.meta, self.exclude = sanitize_meta(
+            meta, char, exclude_char, override_exclude
+        )
 
     def __repr__(self):
         """
         Gives a print representation for the class.
         """
         if self.type is not None:
-            return f"YAML Entry [{self.key}: {self.value}]\n\t Meta: {self.meta}\n\t Type: {self.type}"
+            return (
+                f"YAML Entry [{self.key}: {self.value}]\n\t Meta: {self.meta}\n\t Type:"
+                f" {self.type}"
+            )
         else:
             return f"YAML Entry [{self.key}: {self.value}]\n\t Meta: {self.meta}"
 
@@ -114,6 +200,11 @@ class Entry:
         Arguments:
             schema: Print with four columns instead of three.
         """
+
+        # If the entry is excluded, we don't want to print it.
+        if self.exclude:
+            return ""
+        
         if schema:
             m = "<br />".join(textwrap.wrap(self.meta, width=50))
             if self.type == None:
--- yamldoc/parser.py
+++ yamldoc/parser.py
@@ -2,7 +2,7 @@ import yamldoc.entries
 from datetime import date
 
 
-def parse_yaml(file_path, char="#'", debug=False):
+def parse_yaml(file_path, char="#'", debug=False, exclude_char="#'!", override_exclude=False):
     """
     Parse a YAML file and return a list of YAML classes.
 
@@ -10,13 +10,22 @@ def parse_yaml(file_path, char="#'", debug=False):
         file_path: Path to the YAML file.
         char: A character string used to identify yamldoc blocks.
         debug: Print debug information
+        exclude_char: A character string used to identify blocks to exclude.
+
 
     Return:
         List of YAML blocks.
     """
-    # YAML files have key value pairings seperated by
-    # newlines. The most straightforward kind of things to parse will be
-    # keyvalue pairs preceeded by comments with the Doxygen marker #'
+
+    # The parser works as follows:
+    #   YAML files have key value pairings seperated by
+    #   newlines. The most straightforward kind of things to parse will be
+    #   keyvalue pairs preceeded by comments with the Doxygen marker #'
+    #   (or whatever the user specifies). The parser will look for these
+    #   comments and then parse the key value pairs that follow. The
+    #   parser will then look for the next comment and repeat the
+    #   process. The parser will also look for a comment that starts with
+    #   the exclude_char and ignore the following block.
 
     current_entry = None
     meta = ""
@@ -24,42 +33,84 @@ def parse_yaml(file_path, char="#'", debug=False):
 
     with open(file_path) as yaml:
         for line in [l for l in yaml.readlines() if l.rstrip()]:
-            if debug: print(line.rstrip())
+            if debug:
+                print(line.rstrip())
 
             if current_entry is not None:
                 if current_entry.isBase:
-
                     # If we're back at 0 indentation, the
                     # block is done and we need to quit.
-                    if len(line) - len(line.lstrip(' ')) == 0:
+                    if len(line) - len(line.lstrip(" ")) == 0:
                         # Is current_entry a list or a meta-entry?
                         if current_entry.is_list():
-                            if debug: print("@\tAdding list entry to things.")
+                            if debug:
+                                print("@\tAdding list entry to things.")
                             things.append(current_entry.to_list_entry())
                         else:
-                            if debug: print("@\tAdding meta entry to things.")
+                            if debug:
+                                print("@\tAdding meta entry to things.")
                             things.append(current_entry)
                         current_entry = None
-                        
 
                     # If not, continue parsing the sub entries.
-                    # 
+                    #
                     if current_entry is not None:
-                        if line.lstrip(' ').startswith(char):
-                            meta = meta + line.lstrip().lstrip(char).rstrip()
+                        if line.lstrip(" ").startswith(char) or line.lstrip(
+                            " "
+                        ).startswith(exclude_char):
+                            meta = meta + line.lstrip().rstrip()
                         else:
                             try:
                                 key, value = line.lstrip().rstrip().split(":", 1)
-                                current_entry.entries.append(yamldoc.entries.Entry(key, value.lstrip(' '), meta.lstrip()))
-                                if debug: print("@\tFound an entry and deposited it in meta.")
+
+                                if not value.lstrip():
+                                    new_entry = yamldoc.entries.MetaEntry(
+                                        key, meta, char, exclude_char, override_exclude
+                                    )
+                                else:
+                                    new_entry = yamldoc.entries.Entry(
+                                        key,
+                                        value.lstrip(" "),
+                                        meta.lstrip(),
+                                        char,
+                                        exclude_char,
+                                        override_exclude
+                                    )
+
+                                current_entry.entries.append(new_entry)
+
+                                if debug:
+                                    print("@\tFound an entry and deposited it in meta.")
                                 meta = ""
                             except ValueError:
                                 # If there's only one value, it's a list.
-                                # in this case, we add ths value to the 
+                                # in this case, we add ths value to the
                                 # current entry and continue.
-                                if debug: print("@\tFound a list entry.")
-                                current_entry.entries.append(yamldoc.entries.ListElement(line.lstrip().lstrip("-").lstrip().rstrip()))
+                                if debug:
+                                    print("@\tFound a list entry.")
 
+                                # Have to figure out if this is an element of 
+                                # a nested list or a new list.
+
+                                if len(current_entry.entries) != 0:
+                                    if isinstance(current_entry.entries[-1], yamldoc.entries.MetaEntry):
+                                        current_entry.entries[-1].entries.append(
+                                            yamldoc.entries.ListElement(
+                                                line.lstrip().lstrip("-").lstrip().rstrip()
+                                            )
+                                        )
+                                    else: 
+                                        current_entry.entries.append(
+                                            yamldoc.entries.ListElement(
+                                                line.lstrip().lstrip("-").lstrip().rstrip()
+                                            )
+                                        )
+                                else:
+                                    current_entry.entries.append(
+                                        yamldoc.entries.ListElement(
+                                            line.lstrip().lstrip("-").lstrip().rstrip()
+                                        )
+                                    )
 
             # Either we haven't started yet
             # or we've just flushed the entry.
@@ -73,7 +124,7 @@ def parse_yaml(file_path, char="#'", debug=False):
                 if nspaces == 0:
                     current_entry = None
                     if line.startswith(char):
-                        meta = meta + line.lstrip(char).rstrip()
+                        meta = meta + line.rstrip()
                     else:
                         key, value = line.rstrip().split(":", 1)
 
@@ -83,7 +134,9 @@ def parse_yaml(file_path, char="#'", debug=False):
                         # This could also be a simple list entry but I can't
                         # know that until I see the next line.
                         if not value.lstrip():
-                            current_entry = yamldoc.entries.MetaEntry(key, meta)
+                            current_entry = yamldoc.entries.MetaEntry(
+                                key, meta, char, exclude_char, override_exclude
+                            )
 
                             # Metadata must be flushed after being added as a parent
                             # element
@@ -97,23 +150,28 @@ def parse_yaml(file_path, char="#'", debug=False):
                         else:
                             things.append(
                                 yamldoc.entries.Entry(
-                                    key, value.lstrip(" "), meta.lstrip()
+                                    key,
+                                    value.lstrip(" "),
+                                    meta.lstrip(),
+                                    char,
+                                    exclude_char,
+                                    override_exclude
                                 )
                             )
                             if debug:
                                 print("@\tFound an entry.")
                             meta = ""
 
-
         # The file might run out
         # before the final meta
         # entry is added.
         try:
-            if current_entry.isBase:
-                if current_entry.is_list():
-                    things.append(current_entry.to_list_entry())
-                else:
-                    things.append(current_entry)
+            if current_entry is not None:
+                if current_entry.isBase:
+                    if current_entry.is_list():
+                        things.append(current_entry.to_list_entry())
+                    else:
+                        things.append(current_entry)
         except AttributeError:
             pass
 
@@ -187,7 +245,6 @@ def parse_schema(path_to_file, debug=False):
                 key = key_value(line)
                 value = None
 
-
             # This is awkwardly placed but we have to deal
             # with a special case where lines
             # start with a dash, and indicate that
@@ -202,8 +259,8 @@ def parse_schema(path_to_file, debug=False):
                     special_type_case = False
                 elif line.lstrip(" ").startswith("-"):
                     value = line.lstrip(" ").lstrip("- ").rstrip()
-                    current[parent][name].append(value) # noqa: F821
-                    indents[parent][name].append(indent[1]) # noqa: F821
+                    current[parent][name].append(value)  # noqa: F821
+                    indents[parent][name].append(indent[1])  # noqa: F821
                     continue
                 else:
                     raise TypeError("You must specify a value for type.")
@@ -425,6 +482,8 @@ def main(
     char="#'",
     debug=False,
     schema_path=None,
+    exclude_char="#'!",
+    override_exclude = False,
     title="Configuration Parameters Reference",
     description="Any information about this page goes here.",
     footer=True,
@@ -451,7 +510,7 @@ def main(
     # variables.
     if schema_path is not None:
         schema, specials, extras = parse_schema(schema_path, debug)
-        yaml = parse_yaml(yaml_path, char, debug)
+        yaml = parse_yaml(yaml_path, char, debug, exclude_char, override_exclude)
 
         # Edit the yaml in place with type information.
         add_type_metadata(schema, yaml, debug)
@@ -467,8 +526,8 @@ def main(
 
         # Build the table with top level yaml
 
-        # We only need to print this if there's no 
-        # top level variable first 
+        # We only need to print this if there's no
+        # top level variable first
         if not yaml[0].isBase:
             print("| Key | Value | Type | Information |")
             print("| :-: | :-: | :-: | :-- |")
@@ -492,7 +551,7 @@ def main(
     else:
         print("# " + title + "\n\n" + description + "\n")
 
-        yaml = parse_yaml(yaml_path, char, debug)
+        yaml = parse_yaml(yaml_path, char, debug, exclude_char, override_exclude)
         # Build the table with top level yaml
 
         # We only need to print this if there's no
@@ -500,7 +559,7 @@ def main(
         if not yaml[0].isBase:
             print("| Key | Value | Information |")
             print("| :-: | :-: | :-- |")
-        
+
         for value in yaml:
             if not value.isBase:
                 print(value.to_markdown())
